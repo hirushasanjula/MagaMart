0309e1a28f569985f27b68b7aa893db9dc38f1e7
bounci scot <10898650@students.plymouth.ac.uk>
2024-12-21 16:50:50 +0530

    edited new props

--- app/(storefront)/product/[id]/page.tsx
+++ app/(storefront)/product/[id]/page.tsx
@@ -30,11 +30,18 @@ async function getData(productId: string) {
     return data;
 }
 
-export default async function PorductIdRoute({
- params,
-}: {
-  params: { id: string };
-}) {
+interface PageProps {
+    params: {
+      id: string;
+    };
+  }
+
+export default async function PorductIdRoute({params }: PageProps) {
+
+    if (!params || !params.id) {
+        return notFound();
+      }
+
     noStore();
     const data = await getData(params.id)
     const addProducttoShoppingCart = addItem.bind(null, data.id)
